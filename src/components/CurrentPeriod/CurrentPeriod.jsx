6588ab338125cb3157c8a14284dd4a2ad951378a
pra78 <66877961+pra78@users.noreply.github.com>
2023-05-24 11:45:25 +0300

    luxon lib installed, dt.plus/dt.minus OK

--- src/components/CurrentPeriod/CurrentPeriod.jsx
+++ src/components/CurrentPeriod/CurrentPeriod.jsx
@@ -1,5 +1,32 @@
+import { DateTime } from "luxon";
+import ArrowBackIosIcon from '@mui/icons-material/ArrowBackIos';
+import ArrowForwardIosIcon from '@mui/icons-material/ArrowForwardIos';
+import { Button } from "@mui/material";
+
 const CurrentPeriod = () => {
-    return (<span>May 2023</span>);
+    const date = DateTime.local()
+    var dt = date;
+    // var dt = date.minus({ months: 1 });
+    // dt = dt.plus({ months: 1 });
+    // console.log(dt < date);
+    const dtWeb = dt.toFormat('LLLL yyyy').toUpperCase();
+
+    const handleMonthDecrement = () => {
+        dt = dt.minus({ months: 1 });
+        const dtAPI = dt.toFormat('yyyy-LL');
+        return dtWeb
+    }
+
+    const handleMonthIncrement = () => {
+        dt = dt.plus({ months: 1 });
+    }
+
+    return (
+        <>
+            <Button variant="text" startIcon={<ArrowBackIosIcon />} onClick={handleMonthDecrement}/>
+            <span>{dtWeb}</ span>
+            <Button variant="text" endIcon={<ArrowForwardIosIcon />} onClick={handleMonthIncrement}/>
+        </>);
 };
 
 export default CurrentPeriod;
