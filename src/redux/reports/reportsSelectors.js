0c931cc75031b302a3554147a1ea989a53760463
Artem <ivanichenko.ua@gmail.com>
2023-05-24 15:57:11 +0300

    test

--- src/redux/reports/reportsSelectors.js
+++ src/redux/reports/reportsSelectors.js
@@ -1,1 +1,4 @@
 import { createSelector } from '@reduxjs/toolkit';
+
+export const selectIncomesTotal = state => state.incomes.total;
+export const selectExpensesTotal = state => state.expenses.total;
