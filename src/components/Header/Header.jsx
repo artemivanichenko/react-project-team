4cb1446492bc6bf6c9df1017723aeeaada424862
YuliiaSliepchenko <115469039+YuliiaSliepchenko@users.noreply.github.com>
2023-05-25 13:50:17 +0300

    change1

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,10 +1,10 @@
 import logo from 'images/logo.png';
-import vectorhead from 'images/vectorhead.png';
+
 import OutputOutlinedIcon from '@mui/icons-material/OutputOutlined';
 import Avatar from '@mui/material/Avatar';
 import { useDispatch } from 'react-redux';
 import { logoutUser } from 'redux/auth/authOperations';
-import { Conteiner, Navigate, Title, Btn, Logout, Psevdo,HeaderBgr } from './Header.styled'
+import { Conteiner, Navigate, Title, Btn, Logout, HeaderBgr } from './Header.styled'
 import { useSelector } from 'react-redux';
 import { grey } from '@mui/material/colors';
 
@@ -41,7 +41,6 @@ const Header = () => {
           </OutputOutlinedIcon>
           </Logout>
         <Btn  onClick={handleLogout} type="button" >
-          <Psevdo src={vectorhead} alt="logo" />
           Exit
         </Btn>
       </Navigate>
