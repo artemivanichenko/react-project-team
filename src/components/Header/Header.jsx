1e093154a338f87d1b3575a5d90bfd561f50dbc3
YuliiaSliepchenko <115469039+YuliiaSliepchenko@users.noreply.github.com>
2023-05-25 11:39:16 +0300

    done

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,6 +1,13 @@
 import logo from 'images/logo.png';
+import vectorhead from 'images/vectorhead.png';
+import OutputOutlinedIcon from '@mui/icons-material/OutputOutlined';
+import Avatar from '@mui/material/Avatar';
 import { useDispatch } from 'react-redux';
 import { logoutUser } from 'redux/auth/authOperations';
+import { Conteiner, Navigate, Title, Btn,Logout, Psevdo } from './Header.styled'
+import { grey } from '@mui/material/colors';
+
+
 
 const Header = () => {
   const dispatch = useDispatch();
@@ -10,15 +17,32 @@ const Header = () => {
   };
 
   return (
-    <header>
+    <Conteiner>
       <img src={logo} alt="logo" />
-      <div>
-        <p>UserName</p>
-        <button onClick={handleLogout} type="button">
+      <Navigate>
+        <Avatar sx={{ width: 32, height: 32,margin:1.6 ,marginRight:-5, fontSize:14, background:grey
+}}>U</Avatar>
+        {/* <User src={user} alt="logo" /> */}
+        <Title>
+          UserName
+        </Title>
+        <Logout
+          onClick={handleLogout}
+          type="button">
+        <OutputOutlinedIcon
+          sx={{width:24, height:24, margin: 2.3, marginRight:0}}
+          htmlColor="white"
+          alt="logout"
+          >
+          </OutputOutlinedIcon>
+          </Logout>
+        <Btn  onClick={handleLogout} type="button" >
+          <Psevdo src={vectorhead} alt="logo" />
           Exit
-        </button>
-      </div>
-    </header>
+        </Btn>
+      </Navigate>
+      
+      </Conteiner>
   );
 };
 
