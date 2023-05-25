4ea1341bc02a3896e1229ed95053a41a7c28d49e
Artem <ivanichenko.ua@gmail.com>
2023-05-25 12:17:28 +0300

    test 3

--- src/components/CurrentPeriod/CurrentPeriod.jsx
+++ src/components/CurrentPeriod/CurrentPeriod.jsx
@@ -1,37 +1,47 @@
-import { DateTime } from "luxon";
+import { DateTime } from 'luxon';
 import ArrowBackIosIcon from '@mui/icons-material/ArrowBackIos';
 import ArrowForwardIosIcon from '@mui/icons-material/ArrowForwardIos';
-import { Button } from "@mui/material";
-import { useDispatch } from "react-redux";
-import { getTransactionPeriod } from "redux/reports/reportsOperations";
-import { useEffect, useState } from "react";
-
+import { Button } from '@mui/material';
+import { useDispatch } from 'react-redux';
+import { getTransactionPeriod } from 'redux/reports/reportsOperations';
+import { useEffect, useState } from 'react';
 
 const CurrentPeriod = () => {
-    const dispatch = useDispatch();
-    const [reportPeriod, setReportPeriod] = useState(DateTime.now());
+  const dispatch = useDispatch();
+  const [reportPeriod, setReportPeriod] = useState(DateTime.now());
 
-    useEffect(() => {
-        dispatch(getTransactionPeriod(reportPeriod.toFormat('yyyy-LL')));
-    }, [dispatch, reportPeriod]);
+  useEffect(() => {
+    dispatch(getTransactionPeriod(reportPeriod.toFormat('yyyy-LL')));
+  }, [dispatch, reportPeriod]);
 
-    const handleMonthDecrement = () => {
-        setReportPeriod(prev => prev.minus({ month: 1 }));
-    };
+  const handleMonthDecrement = () => {
+    setReportPeriod(prev => prev.minus({ month: 1 }));
+  };
 
-    const handleMonthIncrement = () => {
-        setReportPeriod(prev => (prev.plus({ month: 1 })));
-    }
+  const handleMonthIncrement = () => {
+    setReportPeriod(prev => prev.plus({ month: 1 }));
+  };
 
-    return (
-        <>
-            <Button variant="text" startIcon={<ArrowBackIosIcon />} onClick={handleMonthDecrement} />
-            <span>{reportPeriod.toFormat('LLLL yyyy').toUpperCase()}</ span>
-            <Button variant="text"
-                endIcon={<ArrowForwardIosIcon />}
-                onClick={handleMonthIncrement}
-                disabled={reportPeriod.startOf('month') < DateTime.now().startOf('month') ? false : true} />
-        </>);
+  return (
+    <>
+      <Button
+        variant="text"
+        startIcon={<ArrowBackIosIcon />}
+        onClick={handleMonthDecrement}
+      />
+      <span>{reportPeriod.toFormat('LLLL yyyy').toUpperCase()}</span>
+      <Button
+        variant="text"
+        endIcon={<ArrowForwardIosIcon />}
+        onClick={handleMonthIncrement}
+        disabled={
+          reportPeriod.startOf('month') < DateTime.now().startOf('month')
+            ? false
+            : true
+        }
+      />
+    </>
+  );
 };
 
 export default CurrentPeriod;
--- src/redux/reports/reportsSelectors.js
+++ src/redux/reports/reportsSelectors.js
@@ -3,3 +3,5 @@ export const selectReportsIsLoading = state => state.reports.isLoading;
 
 export const selectIncomesTotal = state => state.reports.incomes.incomeTotal;
 export const selectExpensesTotal = state => state.reports.expenses.expenseTotal;
+export const selectIncomesData = state => state.reports.incomes.incomesData;
+export const selectExpensesData = state => state.reports.expenses.expensesData;
