33582e7d7162ddeecf643ade19505cee960f21df
Anna-Mariia <122801694+bulo4ka032@users.noreply.github.com>
2023-05-29 13:55:30 +0300

    styled mobile modal

--- src/components/Modal/Modal.styled.jsx
+++ src/components/Modal/Modal.styled.jsx
@@ -1,16 +1,23 @@
-import styled from "@emotion/styled";
+import styled from '@emotion/styled';
 
 export const Overlay = styled.div`
   position: fixed;
-  top: 0;
+  top: 56px;
   left: 0;
   width: 100vw;
   height: 100vh;
-  display: flex;
+  /* display: flex; */
   justify-content: center;
   align-items: center;
-  background-color: rgba(0, 0, 0, 0.8);
-  z-index: 1200;
+  background-color: rgba(0, 0, 0,);
+  z-index: 120;
+  @media screen and (max-width: 767px){
+    background: radial-gradient(
+      320px at left top,
+      rgba(176, 232, 185, 0.8),
+      rgba(56, 56, 71)
+    );
+  }
 `;
 
 export const ModalBox = styled.div`
@@ -22,4 +29,17 @@ export const ModalBox = styled.div`
     rgba(56, 56, 71, 0.29) 107.94%
   );
   box-shadow: 0px 20px 40px rgba(0, 0, 0, 0.2);
+`;
+
+export const StyledBtn = styled.button`
+  margin: 16px 66px;
+  border: none;
+  outline: none;
+  background: transparent;
+  color: #f6f7fb;
+  display: flex;
+  align-items: center;
+  text-transform: uppercase;
+  font-weight: 700;
+font-size: 12px;
 `;
--- src/components/TransactionTypeTabs/TransactionTypeTabs.styled.jsx
+++ src/components/TransactionTypeTabs/TransactionTypeTabs.styled.jsx
@@ -1,5 +1,6 @@
 import styled from '@emotion/styled';
 import { NavLink } from 'react-router-dom';
+import { Tabs, TabList } from 'react-tabs';
 
 export const NavLinkStyled = styled(NavLink)`
   display: block;
@@ -18,7 +19,6 @@ export const NavLinkStyled = styled(NavLink)`
   border-top-right-radius: 20px;
   border-top-left-radius: 20px;
   color: #f6f7fb;
-
   &:last-of-type {
     margin: -1px;
   }
@@ -27,6 +27,18 @@ export const NavLinkStyled = styled(NavLink)`
     background: #383847;
     color: #60c470;
   }
+  @media screen and (max-width: 767px) {
+/* width: 50vw; */
+width: 50vw;
+
+padding: 20px 0;
+ &.active {
+  border: 1px  solid #60C470
+  /* box-shadow: 0px 0px 6px #41A350; */
+ }
+
+}
+
 `;
 
 export const WrapStyled = styled.div`
@@ -37,8 +49,8 @@ export const WrapStyled = styled.div`
 `;
 
 export const mobileTab = styled(NavLink)`
-  position: sticky;
-  bottom: 0;
+  position: fixed;
+  bottom: 0%;
   display: block;
   width: 138px;
   padding: 13px 0;
@@ -55,8 +67,29 @@ export const mobileTab = styled(NavLink)`
   border-top-right-radius: 20px;
   border-top-left-radius: 20px;
   color: #f6f7fb;
+  @media screen and (max-width: 767px) {
+  position: fixed;bottom: 0;
+width: 50vw;
+
+}
 
   &:last-of-type {
     margin: -1px;
   }
 `;
+
+export const TabsStyled = styled(Tabs)`
+
+@media screen and (max-width: 767px) {
+  position: fixed;bottom: 0;
+ max-width: 100vw;
+
+}
+
+`
+
+export const TabsListStyled = styled(TabList)`
+display: flex;
+flex-wrap: nowrap;
+max-width: 100vw;
+`
